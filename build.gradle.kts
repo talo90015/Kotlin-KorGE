3f6e5232dee79e1b72e2299fb202e8490398101c
Talo <talo90015@gmail.com>
2022-09-01 14:26:57 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import com.soywiz.korge.gradle.*
+
+plugins {
+	alias(libs.plugins.korge)
+}
+
+korge {
+	id = "com.sample.Demo"
+	supportBox2d()
+// To enable all targets at once
+
+	//targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+
+	targetJvm()
+	//targetJs()
+	//targetDesktop()
+	//targetIos()
+	//targetAndroidIndirect() // targetAndroidDirect()
+}
